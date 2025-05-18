548d7e11011fb741734df39eb8cdda0940de1e29
Nguyen-thuc526 <nguyennt.itse@gmail.com>
2025-05-18 13:05:41 +0700

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -381,7 +381,7 @@ const Navbar = () => {
                                     padding: 10
                                 }}
                                 onClick={() => {
-                                    navigate(`/song-detail//${item._id}`);
+                                    navigate(`/song-detail/${item._id}`);
                                     handleCloseModal();
                                 }}
                             >
